a5804c1185bb16bc0c0fa83dc10801e7b03a672a
Fredrick Lani <fredricklani@gmail.com>
2023-03-17 00:05:11 +0300

    using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
+/* main - Entry point */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle")
+char use_puts[] = "\"Programming is like building a multilingual puzzle";
 
-return 0
+puts(use_puts);
+
+return (0);
 }
