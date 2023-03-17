d078eb914005377811cdaef9008eb3ab940173fe
Fredrick Lani <fredricklani@gmail.com>
2023-03-17 23:40:03 +0300

    a to z

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success
+ */
+
+int main(void)
+{
+	char a[26] = "abcdefghijklmnopqrstuvwxyz";
+	char b[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(a[i]);
+	}
+	for (x = 0; x < 26; x++)
+	{
+		putchar(b[x]);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
