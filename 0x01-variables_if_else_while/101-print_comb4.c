4f9b4911cfaf7eb4e9b881dd0090e64e04902102
Fredrick Lani <fredricklani@gmail.com>
2023-03-21 00:03:11 +0300

    triple combination

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			for (c = 0; c < 10; c++)
+			{
+				if (a < b && b < c)
+				{
+					putchar(a + '0');
+					putchar(b + '0');
+					putchar(c + '0');
+
+					if (c == 9)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
