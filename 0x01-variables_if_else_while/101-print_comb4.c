f051d9d8d5814a9adf072ddc75396c13590184ef
Fredrick Lani <fredricklani@gmail.com>
2023-03-21 00:07:17 +0300

    triple combination

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,9 +13,9 @@ int main(void)
 
 	for (a = 0; a < 10; a++)
 	{
-		for (b = 0; b < 10; b++)
+		for (b = 1; b < 10; b++)
 		{
-			for (c = 0; c < 10; c++)
+			for (c = 2; c < 10; c++)
 			{
 				if (a < b && b < c)
 				{
