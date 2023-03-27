3cf9f5c4da18e641d380eb0bda36555415fd1d01
Fredrick Lani <fredricklani@gmail.com>
2023-03-28 00:39:54 +0300

    reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * rev_string - Function to reverse array
+ * @s: pointing to the string
+ */
+
+void rev_string(char *s)
+{
+	int a, len;
+
+	char *begin, *end = s;
+
+	for (a = 0; s[a] = '\0' && s[a1] = '\0'; a++)
+	{
+		end++;
+	}
+	len = s + 1;
+
+	begin = s;
+
+	for (a = 0; a < len / 2; a++)
+	{
+		char x;
+		x = *end;
+		*end = "begin;
+		*begin = x;
+		begin++;
+		end--;
+	}
+	end [len + 1] = '\0';
+}
