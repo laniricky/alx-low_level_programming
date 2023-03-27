c2c1646b2d1c749f94bc4b09f9088545f2630c5b
Fredrick Lani <fredricklani@gmail.com>
2023-03-28 00:10:01 +0300

    Reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints reverse
+ * @s: The string
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		continue;
+	}
+	for (i = s[i]; i <= 0; i--)
+	{
+		_putchar(s[i]);
+	}
