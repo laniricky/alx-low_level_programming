9e8a745092b45ab878473745a5065b6c60c300be
Fredrick Lani <fredricklani@gmail.com>
2023-04-17 23:56:41 +0300

    define a new struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Dog's structural information
+ * @name: name member
+ * @age: age member
+ * @owner: owner member
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
