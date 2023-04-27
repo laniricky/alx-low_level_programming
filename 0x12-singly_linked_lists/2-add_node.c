f77a3a42ff0a5182789b78db4bd14bfa59cbc6ac
Fredrick Lani <fredricklani@gmail.com>
2023-04-27 23:15:36 +0300

    Add a new node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -21,10 +21,11 @@ list_t *add_node(list_t **head, const char *str)
 	}
 
 	new = malloc(sizeof(list_t));
-	if (!new){
+	if (!new)
+	{
 		return (NULL);
 	}
-	
+
 	new->str = strdup(str);
 	new->len = len;
 	new->next = (*head);
